53e7017fe3a58fc3a03b36d44f77a155f229c11d
Gaetan Compigni <51131564+techmefr@users.noreply.github.com>
2025-11-28 12:37:10 +0100

    Add ReportStatus component for report status display

---
+++ front/layers/noa-reports/app/components/ReportStatus.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-chip
+    :color="statusColor"
+    size="small"
+    variant="flat"
+  >
+    {{ statusText }}
+  </v-chip>
+</template>
+
+<script setup lang="ts">
+import type { ReportStatus } from '../../types'
+
+const props = defineProps<{
+  status: ReportStatus
+}>()
+
+const statusColor = computed(() => {
+  switch (props.status.slug) {
+    case 'draft':
+      return 'warning'
+    case 'published':
+      return 'success'
+    case 'archived':
+      return 'grey'
+    default:
+      return 'info'
+  }
+})
+
+const statusText = computed(() => props.status.name)
+</script>
