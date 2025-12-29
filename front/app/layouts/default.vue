a037af7c0f4768d1e2c7f58f1348f0ce3995b183
techmefr <compigni.gaetan@gmail.com>
2025-12-29 01:51:52 +0100

    feat(layout): add Vuetify navbar with navigation

--- front/app/layouts/default.vue
+++ front/app/layouts/default.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+const navItems = [
+  { title: 'Home', to: '/', icon: 'mdi-home' },
+  { title: 'Users', to: '/users', icon: 'mdi-account-group' },
+  { title: 'Products', to: '/products', icon: 'mdi-package-variant-closed' }
+]
+</script>
+
 <template>
-  <slot />
+  <v-app-bar color="primary" density="compact">
+    <v-app-bar-title>DDD Demo</v-app-bar-title>
+    <template #append>
+      <v-btn
+        v-for="item in navItems"
+        :key="item.to"
+        :to="item.to"
+        :prepend-icon="item.icon"
+        variant="text"
+      >
+        {{ item.title }}
+      </v-btn>
+    </template>
+  </v-app-bar>
+  <v-main>
+    <slot />
+  </v-main>
 </template>
